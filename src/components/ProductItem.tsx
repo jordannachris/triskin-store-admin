c663bed8df46ac72172ad838404287098443f287
jordannacosta-fluxo <jordanna@fluxo.tec.br>
2025-06-29 20:34:27 -0300

    style ProductItem

--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -16,17 +16,27 @@ const ProductItem = ({ product }: ProductItemProps) => {
     const { name, price, status } = product;
     const statusText = status ? "Ativo" : "Inativo";
     const productImage = status ? productActiveImage : productInactiveImage;
+    const borderColor = status ? "border-l-green-500" : "border-l-red-500";
 
     return (
-        <li className="flex items-center mb-4 border border-fuchsia-800 rounded-lg p-4 bg-pink-50">
-            <div>
-                <img src={productImage} alt={name} className="w-16 h-16 object-contain mx-4" />
+        <li className={`flex items-center mb-4 border border-fuchsia-800 rounded-lg p-4 bg-pink-50 border-l-8 ${borderColor}`}>
+            <div className="w-16 h-16 mx-4">
+                <img src={productImage} alt={name} className="object-contain w-full h-full" />
             </div>
 
-            <div>
-                <p>Nome do produto: {name}</p>
-                <p>Preço: R$ {price}</p>
-                <p>Status: {statusText}</p>
+            <div className="ml-2">
+                <p>
+                    <span className="font-semibold text-fuchsia-800">Nome do produto:</span>
+                    <span className="text-violet-900 ml-1">{name}</span>
+                </p>
+                <p>
+                    <span className="font-semibold text-fuchsia-800">Preço:</span>
+                    <span className="text-violet-900 ml-1">R$ {price}</span>
+                </p>
+                <p>
+                    <span className="font-semibold text-fuchsia-800">Status:</span>
+                    <span className="text-violet-900 ml-1">{statusText}</span>
+                </p>
             </div>
         </li>
     );
