ca5b33eebb161c359f2b02229832c3868eaffed8
jordannacosta-fluxo <jordanna@fluxo.tec.br>
2025-06-30 07:05:37 -0300

    update ProductItem

--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -1,49 +1,52 @@
 import productActiveImage from "../assets/images/product_active.png"
 import productInactiveImage from "../assets/images/product_inactive.png";
 import formatPrice from "../utils/formatPrice";
-import AddProductButton from "./AddProductButton";
+import AddButton from "./AddButton";
 import { ProductInterface } from "../interfaces/ProductInterface";
 import { useCart } from "../contexts/CartContext";
+import ProductInfo from "./ProductInfo";
+import ProductImage from "./ProductImage";
+import EditButton from "./EditButton";
+import DeleteButton from "./DeleteButton";
 
 export interface ProductItemProps {
     product: ProductInterface;
+    isCart?: boolean;
 }
 
-const ProductItem = ({ product }: ProductItemProps) => {
-    const { addToCart } = useCart();
+const ProductItem = ({ product, isCart = false }: ProductItemProps) => {
+    const { addToCart, setProductQuantity } = useCart();
     const { id, name, price, status } = product;
     const statusText = status ? "Ativo" : "Inativo";
     const productImage = status ? productActiveImage : productInactiveImage;
-    const borderColor = status ? "border-l-green-500" : "border-l-red-500";
     const formattedPrice = formatPrice(price);
+
+    const borderColor = status ? "border-l-green-500" : "border-l-red-500";
+
+    // Se for página do carrinho, não mostra borda colorida
+    const cardBorder = isCart
+        ? "border border-fuchsia-800"
+        : `border border-fuchsia-800 border-l-8 ${borderColor}`;
 
     return (
         <li
-            className={`flex flex-col sm:flex-row items-center mb-4 border border-fuchsia-800 rounded-lg p-4 bg-pink-50 border-l-8 ${borderColor} max-w-2xl min-h-[9rem] mx-auto`}
+            className={`flex flex-col sm:flex-row items-center mb-4 rounded-lg p-4 bg-pink-50 max-w-2xl min-h-[9rem] mx-auto ${cardBorder}`}
         >
-            <div className="w-16 h-16 mx-4 flex-shrink-0 mb-4 sm:mb-0">
-                <img src={productImage} alt={name} className="object-contain w-full h-full" />
-            </div>
+            <ProductImage productImage={productImage} productName={name} />
 
-            <div className="ml-2 w-full">
-                <p className="break-words max-w-full">
-                    <span className="font-semibold text-fuchsia-800">Nome do produto:</span>
-                    <span className="text-violet-900 ml-1">{name}</span>
-                </p>
-                <p className="break-words max-w-full">
-                    <span className="font-semibold text-fuchsia-800">Preço:</span>
-                    <span className="text-violet-900 ml-1">R$ {formattedPrice}</span>
-                </p>
-                <p className="break-words max-w-full">
-                    <span className="font-semibold text-fuchsia-800">Status:</span>
-                    <span className="text-violet-900 ml-1">{statusText}</span>
-                </p>
-            </div>
+            <ProductInfo productName={name} productPrice={formattedPrice} productStatus={statusText} />
 
-            <AddProductButton
-                status={status}
-                onClick={() => addToCart(id)}
-            />
+            {isCart ? (
+                <div className="flex flex-col gap-2 mt-4 sm:mt-0 sm:ml-6">
+                    <EditButton productId={id} />
+                    <DeleteButton productId={id} onDelete={id => setProductQuantity(id, 0)} />
+                </div>
+            ) : (
+                <AddButton
+                    status={status}
+                    onClick={() => addToCart(id)}
+                />
+            )}
         </li>
     );
 }
