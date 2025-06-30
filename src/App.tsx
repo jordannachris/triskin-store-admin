435633d145673abc3bce40f8036b7e3b0b92eeb5
jordannacosta-fluxo <jordanna@fluxo.tec.br>
2025-06-30 00:28:08 -0300

    remove comments from App

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-// import { loader as productsLoader } from "./pages/HomePage/loader";
 import CartPage from "./pages/CartPage";
 import EditPage from "./pages/EditPage";
 import AppLayout from "./components/AppLayout";
@@ -9,11 +8,10 @@ import ErrorMessage from "./components/ErrorMessage";
 
 const router = createBrowserRouter([
   {
-    element: <AppLayout />, 
+    element: <AppLayout />,
     errorElement: <ErrorMessage />,
     children: [
-      // { path: "/", element: <HomePage />, loader: productsLoader },
-      { path: "/", element: <HomePage />,  errorElement: <ErrorMessage />, },
+      { path: "/", element: <HomePage />, errorElement: <ErrorMessage />, },
       { path: "/cart", element: <CartPage /> },
       { path: "/cart/edit/:productId", element: <EditPage /> },
     ]
