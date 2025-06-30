a4134152bbbe730c7a2a34d18ab072ea8ce4f3fd
jordannacosta-fluxo <jordanna@fluxo.tec.br>
2025-06-30 07:14:13 -0300

    update ProductInfo

--- src/components/ProductInfo.tsx
+++ src/components/ProductInfo.tsx
@@ -1,10 +1,18 @@
 export interface ProductInfoProps {
     productName: string;
     productPrice: string;
-    productStatus: string;
+    productStatus?: string;
+    productQuantity?: number;
+    isCart?: boolean;
 }
 
-const ProductInfo = ({ productName, productPrice, productStatus }: ProductInfoProps) => {
+const ProductInfo = ({
+    productName,
+    productPrice,
+    productStatus,
+    productQuantity,
+    isCart = false,
+}: ProductInfoProps) => {
     return (
         <div className="ml-2 w-full">
             <p className="break-words max-w-full">
@@ -15,10 +23,17 @@ const ProductInfo = ({ productName, productPrice, productStatus }: ProductInfoPr
                 <span className="font-semibold text-fuchsia-800">Preço:</span>
                 <span className="text-violet-900 ml-1">R$ {productPrice}</span>
             </p>
-            <p className="break-words max-w-full">
-                <span className="font-semibold text-fuchsia-800">Status:</span>
-                <span className="text-violet-900 ml-1">{productStatus}</span>
-            </p>
+            {isCart ? (
+                <p className="break-words max-w-full">
+                    <span className="font-semibold text-fuchsia-800">Quantidade:</span>
+                    <span className="text-violet-900 ml-1">{productQuantity}</span>
+                </p>
+            ) : (
+                <p className="break-words max-w-full">
+                    <span className="font-semibold text-fuchsia-800">Status:</span>
+                    <span className="text-violet-900 ml-1">{productStatus}</span>
+                </p>
+            )}
         </div>
     );
 };
