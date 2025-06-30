4f60d13e2af6cf8fbbc204cc0f303ed4d57f122c
jordannacosta-fluxo <jordanna@fluxo.tec.br>
2025-06-30 00:40:35 -0300

    add count to CartIcon

--- src/components/CartIcon.tsx
+++ src/components/CartIcon.tsx
@@ -1,15 +1,25 @@
 import { Link } from "react-router-dom";
 import cartIcon from "../assets/images/cart_icon.svg";
 
-const CartIcon = () => {
+interface CartIconProps {
+    count?: number;
+}
+
+const CartIcon = ({ count = 0 }: CartIconProps) => {
     return (
-        <Link to="/cart" className="mt-3 md:mt-0 block">
+        <Link to="/cart" className="mt-3 md:mt-0 block relative">
             <img
                 src={cartIcon}
                 alt="Carrinho"
                 className="w-14 h-12"
                 draggable={false}
             />
+            <span
+                className="absolute bottom-0 right-0 translate-x-1/4 translate-y-1/4 bg-fuchsia-700 text-white text-xs font-bold rounded-full w-6 h-6 flex items-center justify-center border-2 border-white pointer-events-none"
+                style={{ minWidth: "1.5rem", minHeight: "1.5rem" }}
+            >
+                {count}
+            </span>
         </Link>
     );
 
