996ab2f2da97a158ed594692e9c4e90e83ca3c40
jordannacosta-fluxo <jordanna@fluxo.tec.br>
2025-06-30 07:05:49 -0300

    update ProductList

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -4,14 +4,19 @@ import ProductItem from "./ProductItem";
 
 export interface ProductListProps {
     products: ProductInterface[];
+    isCart?: boolean;
 }
 
 
-const ProductList = ({ products }: ProductListProps) => {
+const ProductList = ({ products, isCart = false }: ProductListProps) => {
 
     return (
         <div className="container mx-auto px-4">
-            <ul>{products.map((item) => <ProductItem product={item} key={item.id} />)}</ul>
+            <ul>
+                {products.map((item) => (
+                    <ProductItem key={item.id} product={item} isCart={isCart} />
+                ))}
+            </ul>
         </div>
     );
 }
