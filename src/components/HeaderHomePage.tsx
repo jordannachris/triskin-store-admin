e9fdd00416552e3a8b1b897102d7880397401234
jordannacosta-fluxo <jordanna@fluxo.tec.br>
2025-06-29 22:41:43 -0300

    style Header and HeaderHomePage

--- src/components/HeaderHomePage.tsx
+++ src/components/HeaderHomePage.tsx
@@ -8,7 +8,7 @@ interface HeaderProps {
 
 const HeaderHomePage = ({ search, setSearch }: HeaderProps) => {
     return (
-        <header className="bg-gradient-to-r from-fuchsia-900 via-fuchsia-600 to-violet-700 font-semibold text-white p-7 uppercase flex flex-col md:flex-row items-center justify-between gap-4">
+        <header className="bg-gradient-to-r from-fuchsia-900 via-fuchsia-600 to-violet-700 font-semibold text-white p-7 min-h-24 uppercase flex flex-col md:flex-row items-center justify-between gap-4">
             <Link to="/" className="tracking-widest">Triskin Store Admin</Link>
 
             <div className="flex-1 flex justify-center">
