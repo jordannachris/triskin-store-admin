4f2a15c045361babf7fe1c5c33ea63a59a59d7ab
jordannacosta-fluxo <jordanna@fluxo.tec.br>
2025-06-30 01:52:08 -0300

    remove comments from productApi

--- src/services/api/productApi.ts
+++ src/services/api/productApi.ts
@@ -1,8 +1,6 @@
-// const API_URL = 'https://6860cb5f8e7486408444020d.mockapi.io/api/v1/products';
 const API_URL = 'https://6860cb5f8e7486408444020d.mockapi.io/api/v1';
 
 export async function fetchProducts() {
-  // return fetch(API_URL);
    return fetch(`${API_URL}/products`);
 }
 
