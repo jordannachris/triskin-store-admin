3b476fb3b7f361906d8a2c5944453da6b75ecc10
jordannacosta-fluxo <jordanna@fluxo.tec.br>
2025-06-30 07:23:57 -0300

    update AddButton

--- src/components/AddButton.tsx
+++ src/components/AddButton.tsx
@@ -7,7 +7,7 @@ const AddButton = ({ status, onClick }: AddProductButtonProps) => {
     return (
         <div className="flex flex-col items-center mt-4 sm:mt-0 sm:ml-6">
             <button
-                className={`px-4 py-2 rounded-full font-semibold text-sm transition
+                className={`px-9 py-1 rounded-2xl font-semibold text-sm transition
                     ${status
                         ? "bg-fuchsia-700 text-white hover:bg-fuchsia-800"
                         : "bg-gray-300 text-gray-400 cursor-not-allowed opacity-60"
